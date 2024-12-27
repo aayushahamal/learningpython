4ce01861c398843b49bb660357584f43fe0c721c
aayushahamal <aaayushahamal@gmail.com>
2024-12-27 22:06:57 +0545

    Create step 3.py

---
+++ Building a Case Convertor Program/step 3.py
@@ -0,0 +1,8 @@
+#Withthe empty list in place, now you can start iterating through the input string and convert it into snake case.
+#Inside the function, below the list you just created, add a for loop to iterate through the pascal_or_camel_cased_string. Make sure to name the target variable char. For now, add a pass statement as a placeholder in the loop body.
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+           pass
+        
