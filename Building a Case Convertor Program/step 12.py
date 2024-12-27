49cccaedf2f0dac7462d0b4f5763024e31885ba6
aayushahamal <aaayushahamal@gmail.com>
2024-12-27 22:57:17 +0545

    Create step 12.py

---
+++ Building a Case Convertor Program/step 12.py
@@ -0,0 +1,20 @@
+#Inside the main() function, replace the pass statement, with a call to the convert_to_snake_case() function, 
+#passing the string 'aLongAndComplexString' as input.
+#To display the output, pass the function call as the argument to the print() function.
+
+
+def convert_to_snake_case(pascal_or_camel_cased_string):
+    snake_cased_char_list = []
+    for char in pascal_or_camel_cased_string:
+        if char.isupper():
+            converted_character = '_' + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
+    snake_cased_string = ''.join(snake_cased_char_list)
+    clean_snake_cased_string = snake_cased_string.strip('_')
+
+    return clean_snake_cased_string
+
+def main():
+      print(convert_to_snake_case('aLongAndComplexString'))
