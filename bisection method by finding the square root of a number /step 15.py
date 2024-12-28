609d50266182a8530bb8f116ef4732f6fde6ea9e
aayushahamal <aaayushahamal@gmail.com>
2024-12-28 21:30:45 +0545

    Create step 15.py

---
+++ bisection method by finding the square root of a number /step 15.py
@@ -0,0 +1,28 @@
+#If the difference is not within the specified tolerance, create an elif statement to check if square_mid is less than square_target.
+#Assign the value of mid to low as the square root would now lie between low and mid.
+
+
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    if square_target < 0:
+        raise ValueError('Square root of negative number is not defined in real numbers')
+    if square_target == 1:
+        root = 1
+        print(f'The square root of {square_target} is 1')
+    elif square_target == 0:
+        root = 0
+        print(f'The square root of {square_target} is 0')
+
+    else:
+        low = 0
+        high = max(1, square_target)
+        root = None
+        
+        for _ in range(max_iterations):
+            mid = (low + high) / 2
+            square_mid = mid**2
+
+            if abs(square_mid - square_target) < tolerance:
+                root = mid
+            elif square_mid < square_target :
+                low = mid
+                
