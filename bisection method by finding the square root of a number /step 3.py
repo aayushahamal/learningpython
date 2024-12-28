54a827bd7e7a5e30d525e84ba4cbf50406f52c32
aayushahamal <aaayushahamal@gmail.com>
2024-12-28 20:55:28 +0545

    Create step 3.py
    Step 3
    If the number for which you want to find the square root is negative, the code should raise an error as the square root of a negative number is not defined in real numbers.
    
    Remove the pass statement and create an if statement to check if square_target is less than 0.

---
+++ bisection method by finding the square root of a number /step 3.py
@@ -0,0 +1,7 @@
+#Remove the pass statement and create an if statement to check if square_target is less than 0.
+
+
+
+def square_root_bisection(square_target, tolerance = 1e-7, max_iterations = 100):
+    if square_target <0 :
+        pass
