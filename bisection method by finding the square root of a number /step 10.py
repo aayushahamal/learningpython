2b722c67c58e718d90d2dd3cf42d479d2badec33
aayushahamal <aaayushahamal@gmail.com>
2024-12-28 21:13:12 +0545

    Create step 10.py

---
+++ bisection method by finding the square root of a number /step 10.py
@@ -0,0 +1,16 @@
+#Set the value of root to None as at this point, you don't have an approximate value yet.
+
+
+def square_root_bisection(square_target, tolerance=1e-7, max_iterations=100):
+    if square_target < 0:
+        raise ValueError('Square root of negative number is not defined in real numbers')
+    if square_target == 1:
+        root = 1
+        print(f'The square root of {square_target} is 1')
+    elif square_target == 0:
+        root = 0
+        print(f'The square root of {square_target} is 0')
+    else:
+        low = 0
+        high = max(1, square_target)
+        root = None
