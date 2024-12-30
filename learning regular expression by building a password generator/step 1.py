ce2d910c62bc48f3ee3c5545a96c17928d578e21
aayushahamal <aaayushahamal@gmail.com>
2024-12-30 20:42:38 +0545

    Create step 1.py

---
+++ learning regular expression by building a password generator/step 1.py
@@ -0,0 +1,7 @@
+#A Python module is a file containing code designed to perform specific tasks. The Python standard library contains many modules that you can import and use in your code. 
+#You can achieve this by using the import statement followed by the name of the module.
+#Start this project by importing the string module.
+
+
+
+import string
