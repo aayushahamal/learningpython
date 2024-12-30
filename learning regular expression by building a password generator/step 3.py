0af2bd8dccbdb5fee50a509fd5084df4c4cddc79
aayushahamal <aaayushahamal@gmail.com>
2024-12-30 20:46:42 +0545

    Create step 3.py

---
+++ learning regular expression by building a password generator/step 3.py
@@ -0,0 +1,9 @@
+#Declare two new variables named digits and symbols and assign them string.digits and string.punctuation, respectively.
+
+
+
+import string
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
