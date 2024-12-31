1f0025f6f512837e47acf05a395a057763171b0c
aayushahamal <aaayushahamal@gmail.com>
2024-12-31 22:12:48 +0545

    Create step 4.py

---
+++ learning regular expression by building a password generator/step 4.py
@@ -0,0 +1,9 @@
+#These three variables constitute the possible characters to choose from when generating the password.
+#Just before them, add a comment saying Define the possible characters for the password.
+
+
+import string
+#Define the possible characters for the password.
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
