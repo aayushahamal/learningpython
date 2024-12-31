d5360e86cb870323ca98e4d3f35e4cf5a07168fe
aayushahamal <aaayushahamal@gmail.com>
2024-12-31 22:17:26 +0545

    Create step 5.py

---
+++ learning regular expression by building a password generator/step 5.py
@@ -0,0 +1,9 @@
+#Now declare a variable named all_characters and assign it the result of concatenating your existing variables.
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+all_characters = letters + digits + symbols
