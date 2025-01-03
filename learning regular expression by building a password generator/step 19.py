6a2b3002ee23095a72034c757b5969ad50dd99aa
aayushahamal <aaayushahamal@gmail.com>
2025-01-03 21:29:06 +0545

    Create step 19.py

---
+++ learning regular expression by building a password generator/step 19.py
@@ -0,0 +1,19 @@
+#After the loop, add a return statement to your function so it returns the password variable.
+
+import secrets
+import string
+
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+    password = ''
+    # Generate password
+    for _ in range(length):
+        password += secrets.choice(all_characters)
+    return password
