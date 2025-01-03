fa75690832026fcbee3caba9333b9860b968d042
aayushahamal <aaayushahamal@gmail.com>
2025-01-03 21:10:00 +0545

    Create step 12.py
    Step 12
    Although the effect might seem equal to random.choice(), secrets ensure you the most secure source of randomness that your operating system can provide.

---
+++ learning regular expression by building a password generator/step 12.py
@@ -0,0 +1,13 @@
+#Now, delete your two print() calls.
+
+import secrets
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
