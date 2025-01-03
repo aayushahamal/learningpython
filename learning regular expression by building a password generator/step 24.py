4ccecc93e5738d8442fe7c418a1f201420469c38
aayushahamal <aaayushahamal@gmail.com>
2025-01-03 21:42:46 +0545

    Create step 24.py

---
+++ learning regular expression by building a password generator/step 24.py
@@ -0,0 +1,25 @@
+#Put your password variable declaration and the following for loop inside a while loop. Use True as the condition for your new loop.
+
+
+
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+    while True:
+        password = ''
+    # Generate password
+        for _ in range(length):
+          password += secrets.choice(all_characters)
+        return password
+  
+# new_password = generate_password(8)
+# print(new_password)
